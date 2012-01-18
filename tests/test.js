cbc25d50328fefc320bda37fd02e2447bf79047c
Mickael Daniel <daniel.mickael@gmail.com>
2012-01-18 22:57:53 +0100

    edit pkg dependencies and necessary changes for test script
    so that it can be used from outside a posix environment (scafolding
    command, now using rimraf, ncp when possible). necessary to test things
    out on win32

--- tests/test.js
+++ tests/test.js
@@ -1,30 +1,32 @@
 
 var dombuild = require('../'),
+  fs = require('fs'),
   path = require('path'),
-  exec = require('child_process').exec,
-  assert = require('assert');
+  rimraf = require('rimraf'),
+  ncp = require('ncp').ncp,
+  assert = require('assert'),
+  dirname = path.resolve(__dirname, 'fixture/'),
+  tmpdir = path.resolve(__dirname, 'build/');
 
 assert.ok(dombuild, 'Should dombuild required returns something');
 
-
+// change working dir for conveniency
+process.chdir(__dirname);
 
 // clean up previous build dir, and prepare a new fresh version
+rimraf(tmpdir, function(err) {
+  if(err) throw err;
 
-var cmds = [
-  'rm -rf intermediate',
-  'mkdir intermediate',
-  'cp -vr index.html 404.html js/ css/ intermediate/'
-].join(' && ');
+  console.log('Deleted previous build dir...');
+  console.log('Created new build dir...');
 
-exec(cmds, { cwd: path.join(__dirname, '..') }, function(err) {
-  assert.ifError(err);
-  console.log('Intermediate done');
+  ncp(dirname, tmpdir, function(err) {
+    if(err) throw err;
 
-  dombuild(['index.html', '404.html'], { dirname: path.join(__dirname, '../intermediate') }, function(err) {
-    assert.ifError(err);
-
-    console.log('dom build done');
+    dombuild(['index.html', '404.html'], { dirname: tmpdir }, function(err) {
+      assert.ifError(err);
+      console.log('dom build done');
+    });
   });
 
 });
-
